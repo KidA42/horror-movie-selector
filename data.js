fc532f8b034d222e41dbdbc3919995d8e8a13620
Scott Aguila <121623880+KidA42@users.noreply.github.com>
2023-10-26 19:16:58 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,50 @@
+export const moviesData = [
+    {
+        emotionTags: ["ouch, you slashed me", "travelin' through time"],
+        isTerrifying: false,
+        image: "totally-killer.webp",
+        alt: "totally killer movie poster",
+    },
+    {
+        emotionTags: ["ouch, you slashed me", "travelin' through time"],
+        isTerrifying: false,
+        image: "happy-death-day.webp",
+        alt: "happy death day movie poster",
+    },
+     {
+        emotionTags: ["tragic romance", "teleporter troubles"],
+        isTerrifying: false,
+        image: "the-fly.webp",
+        alt: "the fly movie poster",
+    },
+    {
+        emotionTags: ["ew"],
+        isTerrifying: true,
+        image: "in-my-mothers-skin.webp",
+        alt: "in my mother's skin movie poster",
+    },
+    {
+        emotionTags: ["sippin' on android milk", "space cat snuggles", "ew"],
+        isTerrifying: true,
+        image: "alien.webp",
+        alt: "alien movie poster",
+    },
+    {
+        emotionTags: ["blood tests", "danger dog", "ew"],
+        isTerrifying: false,
+        image: "the-thing.webp",
+        alt: "the thing poster",
+    },
+    {
+        emotionTags: ["blood tests", "canines galore"],
+        isTerrifying: false,
+        image: "interview-with-the-vampire.webp",
+        alt: "interview with the vampire poster",
+    },
+    {
+        emotionTags: ["blood tests", "canines galore"],
+        isTerrifying: false,
+        image: "interview-with-the-vampire.webp",
+        alt: "interview with the vampire poster",
+    },
+]
---
+++ index.js
@@ -0,0 +1,105 @@
+import { moviesData } from '/data.js'
+
+const emotionRadios = document.getElementById('emotion-radios')
+const getImageBtn = document.getElementById('get-image-btn')
+const terrifyingFilmsOnlyOption = document.getElementById('terrifying-films-only-option')
+const memeModalInner = document.getElementById('meme-modal-inner')
+const memeModal = document.getElementById('meme-modal')
+const memeModalCloseBtn = document.getElementById('meme-modal-close-btn')
+
+emotionRadios.addEventListener('change', highlightCheckedOption)
+
+memeModalCloseBtn.addEventListener('click', closeModal)
+
+getImageBtn.addEventListener('click', renderMovie)
+
+function highlightCheckedOption(e){
+    const radios = document.getElementsByClassName('radio')
+    for (let radio of radios){
+        radio.classList.remove('highlight')
+    }
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
+
+function closeModal(){
+    memeModal.style.display = 'none'
+}
+
+function renderMovie(){
+    const movieObject = getSingleMovieObject()
+    memeModalInner.innerHTML =  `
+        <img 
+        class="cat-img" 
+        src="./images/${movieObject.image}"
+        alt="${movieObject.alt}"
+        >
+        `
+    memeModal.style.display = 'flex'
+}
+
+function getSingleMovieObject(){
+    const moviesArray = getMatchingMoviesArray()
+    
+    if(moviesArray.length === 1){
+        return moviesArray[0]
+    }
+    else{
+        const randomNumber = Math.floor(Math.random() * moviesArray.length)
+        return moviesArray[randomNumber]
+    }
+}
+
+function getMatchingMoviesArray(){     
+    if(document.querySelector('input[type="radio"]:checked')){
+        const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
+        const isTerrifying = terrifyingFilmsOnlyOption.checked
+        
+        const matchingMoviesArray = moviesData.filter(function(movie){
+            
+            if(isTerrifying){
+                return movie.emotionTags.includes(selectedEmotion) && movie.isTerrifying
+            }
+            else{
+                return movie.emotionTags.includes(selectedEmotion)
+            }            
+        })
+        return matchingMoviesArray 
+    }  
+}
+
+function getEmotionsArray(movies){
+    const emotionsArray = []    
+    for (let movie of movies){
+        for (let emotion of movie.emotionTags){
+            if (!emotionsArray.includes(emotion)){
+                emotionsArray.push(emotion)
+            }
+        }
+    }
+    return emotionsArray
+}
+
+function renderEmotionsRadios(movies){
+        
+    let radioItems = ``
+    const emotions = getEmotionsArray(movies)
+    for (let emotion of emotions){
+        radioItems += `
+        <div class="radio">
+            <label for="${emotion}">${emotion}</label>
+            <input
+            type="radio"
+            id="${emotion}"
+            value="${emotion}"
+            name="emotions"
+            >
+        </div>`
+    }
+    emotionRadios.innerHTML = radioItems
+}
+
+renderEmotionsRadios(moviesData)
+
+
+
+
